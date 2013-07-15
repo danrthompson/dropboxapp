43ad3b29f9d0c452a6354fdc5f616e5e12a0a326
Dan Thompson <dant1@stanford.edu>
2013-07-14 22:03:41 -0600

    added resource dependency on folders

--- siteroot/db/schema.rb
+++ siteroot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130715031043) do
+ActiveRecord::Schema.define(:version => 20130715035812) do
 
   create_table "db_users", :force => true do |t|
     t.datetime "created_at",                             :null => false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20130715031043) do
     t.string   "resource_file_content_type"
     t.integer  "resource_file_file_size"
     t.datetime "resource_file_updated_at"
+    t.integer  "folder_id"
   end
 
 end
