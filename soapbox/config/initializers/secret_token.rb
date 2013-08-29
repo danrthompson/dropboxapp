d14a41d70b1f46c207f8ff633ba1d0fb5fce04d0
Dan Thompson <dant1@stanford.edu>
2013-08-29 10:23:07 -0600

    created fresh app

---
+++ soapbox/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soapbox::Application.config.secret_token = '9548a16cd7f57920f55245fff41769afd883a42ea55a415626c56e4e536c615d5d2b35e21c6bcd697ad4dea35e550b5bfecab69ec7f199ca47ac3ef840541f4b'
