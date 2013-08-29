0c37800cafc11aafb8fccc5f880090ced566b62a
Dan Thompson <dant1@stanford.edu>
2013-08-29 10:57:28 -0600

    devise might be done

--- soapbox/app/models/user.rb
+++ soapbox/app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable, :timeoutable
+  # soon: :confirmable, :lockable, :recoverable, :omniauthable
+  # maybe: :token_authenticatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
